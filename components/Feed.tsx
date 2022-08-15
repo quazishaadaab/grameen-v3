4abc1dcc6f612be0bb509c32fe956666f264696e
Quazi Shaadaab <quazishaadaab@Quazis-MacBook-Air.local>
2022-08-15 18:14:12 -0400

    fixed bugs

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -11,24 +11,27 @@ topic?: string
 
 function Feed( {topic}:Props) {
 
-let data = null ;
 
-if(topic){
-  const { data :topicData, error } = useQuery(GET_ALL_POSTS_BY_TOPIC, {
+let topic_boolean = false ;
+
+topic? (topic_boolean = true ) :( topic_boolean = false ) ;
+
+
+  const { data:speceficData   } = useQuery(GET_ALL_POSTS_BY_TOPIC, {
+skip:!topic_boolean,
 variables: {topic:topic}
-  });
+  } )
 
-  data =topicData ;
-}else{
-  const { data :topicData, error } = useQuery(GET_ALL_POSTS) 
-  data =topicData ;
+  const { data:normalData  } = useQuery(GET_ALL_POSTS, {
+    skip:topic_boolean
+  }) 
 
-}
+
 
 
 
   //if topic doesnt exist, show all posts. if topic exists, show the posts with that topic only
-  const posts: Post[] = !topic ? data?.getPostList : data?.getPostListByTopic; // the response that is returned is a json with getPostList as the title/header/object name
+  const posts: Post[] = !topic ? normalData?.getPostList : speceficData?.getPostListByTopic; // the response that is returned is a json with getPostList as the title/header/object name
   //we specify the type Post[] in typings.d.ts
 
   return (
